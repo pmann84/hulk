562622ff4dee7fee50b2f8b76b8e92443aae1896
pmann84 <peterdmann28@gmail.com>
2021-03-03 23:14:36 +0000

    Setup separate debug patterns for logging

--- hulk/include/hulk/logging.hpp
+++ hulk/include/hulk/logging.hpp
@@ -3,19 +3,24 @@
 
 #include <spdlog/spdlog.h>
 
+#include <string>
+
 namespace hulk
 {
     namespace log
     {
-        void init(bool debug = false)
-        {
-            spdlog::set_pattern("%^[%d/%m/%Y %T.%e][%l][thread %t] %v%$");
-            spdlog::set_level(debug ? spdlog::level::debug : spdlog::level::info); 
-        }
+        constexpr char debug_log_pattern[] = "%^[%d/%m/%Y %T.%e][%l][thread %t] %v%$";
+        constexpr char log_pattern[] = "%^[%d/%m/%Y %T.%e][%l] %v%$";
 
         void set_level(spdlog::level::level_enum level)
         {
-            spdlog::set_level(level);
+            spdlog::set_pattern(level == spdlog::level::debug ? debug_log_pattern : log_pattern);
+            spdlog::set_level(level); 
+        }
+
+        void init(bool debug = false)
+        {
+            set_level(debug ? spdlog::level::debug : spdlog::level::info);
         }
 
         template<typename FormatString, typename... Args>
